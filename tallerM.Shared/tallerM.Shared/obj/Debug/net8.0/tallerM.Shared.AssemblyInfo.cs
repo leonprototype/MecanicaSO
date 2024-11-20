13a5d65b39867f37a18a073140fd867d9ce6aae4
Samantha Falcon <valeria-samantha@hotmail.com>
2024-11-20 13:52:41 -0600

    Revert "Creación web api"
    This reverts commit fc986f90a1a4df25879c5ed527579278ab53b95a.

--- tallerM.Shared/tallerM.Shared/obj/Debug/net8.0/tallerM.Shared.AssemblyInfo.cs
+++ tallerM.Shared/tallerM.Shared/obj/Debug/net8.0/tallerM.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tallerM.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b752885fbd0b09687f1087fedb343c041e9c06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("tallerM.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tallerM.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
